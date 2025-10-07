a1ee0f722aa9e4ea2c647bfaf2430fa15fa304be
elian332324 <elianas@kamukschool.ed.cr>
2025-10-07 13:32:12 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+basic.forever(function () {
+    if (true) {
+        basic.showIcon(IconNames.Sad)
+        wuKong.setServoAngle(wuKong.ServoTypeList._360, wuKong.ServoList.S0, 0)
+    }
+})
